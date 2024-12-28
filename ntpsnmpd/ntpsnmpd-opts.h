ef7670e705e0b0ffe0635873bfed128d5f8c7555
NTP Release Engineering <stenn@ntp.org>
2024-12-28 02:17:01 -0800

    NTP_4_3_107
    bk: 676fd01duiCifVf68zz5EBkyth7L7g

--- ntpsnmpd/ntpsnmpd-opts.h
+++ ntpsnmpd/ntpsnmpd-opts.h
@@ -1,7 +1,7 @@
 /*
  *  EDIT THIS FILE WITH CAUTION  (ntpsnmpd-opts.h)
  *
- *  It has been AutoGen-ed  November 28, 2024 at 12:06:29 AM by AutoGen 5.18.16
+ *  It has been AutoGen-ed  December 28, 2024 at 02:14:48 AM by AutoGen 5.18.16
  *  From the definitions    ntpsnmpd-opts.def
  *  and the template file   options
  *
@@ -84,9 +84,9 @@ typedef enum {
 /** count of all options for ntpsnmpd */
 #define OPTION_CT    8
 /** ntpsnmpd version */
-#define NTPSNMPD_VERSION       "4.3.106"
+#define NTPSNMPD_VERSION       "4.3.107"
 /** Full ntpsnmpd version text */
-#define NTPSNMPD_FULL_VERSION  "ntpsnmpd 4.3.106"
+#define NTPSNMPD_FULL_VERSION  "ntpsnmpd 4.3.107"
 
 /**
  *  Interface defines for all options.  Replace "n" with the UPPER_CASED
